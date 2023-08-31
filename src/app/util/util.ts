31af004b6cd56af5526f6ad7c65cbebaea037d95
Tomas Muñoz <129495947+TomasMu97@users.noreply.github.com>
2023-08-31 12:35:36 +0200

    Update util.ts
    Update the calculations of forward kinematics so that only a DH parameter matrix is needed for each robot.

--- src/app/util/util.ts
+++ src/app/util/util.ts
@@ -1,96 +1,156 @@
-/**
- * function to convert robot joints into spatial (3D = x,y,z)
- */
+// Declare all known DH Parameters
+//Structure:
+/*
+[J1.Alpha,    J1.Theta_offset,      J1.a,      J1.d   ],
+[J2.Alpha,    J2.Theta_offset,      J2.a,      J2.d   ],
+[J3.Alpha,    J3.Theta_offset,      J3.a,      J3.d   ],
+[J4.Alpha,    J4.Theta_offset,      J4.a,      J4.d   ],
+.
+.
+.
+[Jn.Alpha,    Jn.Theta,      Jn.a,      Jn.d   ],
+*/
 
-/**
- * ([q1,q2,q3,q4] --> [x,y,z]):
- 
+const DH_Edscorbot =
+[
+    [-Math.PI/2,    0,      5,      35.85   ],
+    [0,             0,      30,     -9.8    ],
+    [0,             0,      35,     6.5     ],
+    [Math.PI/2,     0,      22,     0       ]
+]
 
-def direcKin(q1,q2,q3,q4):
-    a1 = 5.0
-    a2 = 30
-    a3 = 35
-    a4 = 22
-    d1 = 35.85
-    d2 = -9.8
-    d3 = 6.5
-    x= a1*np.cos(q1) + a2*np.cos(q1)*np.cos(q2) - a3*np.sin(q2)*np.sin(q3)*np.cos(q1) + a3*np.cos(q1)*np.cos(q2)*np.cos(q3) + a4*(-np.sin(q2)*np.sin(q3)*np.cos(q1) + np.cos(q1)*np.cos(q2)*np.cos(q3))*np.cos(q4) + a4*(-np.sin(q2)*np.cos(q1)*np.cos(q3) - np.sin(q3)*np.cos(q1)*np.cos(q2))*np.sin(q4) - d2*np.sin(q1) - d3*np.sin(q1)
+const DH_Robotanno =
+[
+    [Math.PI/2,     0,          35.6,   369     ],
+    [0,             Math.PI/2,  241.5,  0       ],
+    [-Math.PI/2,    -Math.PI,   0,      0       ],
+    [Math.PI/2,     0,          0,      288.2   ],
+    [Math.PI/2,     Math.PI/2,  0,      0       ],
+    [0,             0,          0,      136     ]
+]
 
- 
-
-
-    y= a1*np.sin(q1) + a2*np.sin(q1)*np.cos(q2) - a3*np.sin(q1)*np.sin(q2)*np.sin(q3) + a3*np.sin(q1)*np.cos(q2)*np.cos(q3) + a4*(-np.sin(q1)*np.sin(q2)*np.sin(q3) + np.sin(q1)*np.cos(q2)*np.cos(q3))*np.cos(q4) + a4*(-np.sin(q1)*np.sin(q2)*np.cos(q3) - np.sin(q1)*np.sin(q3)*np.cos(q2))*np.sin(q4) + d2*np.cos(q1) + d3*np.cos(q1)
-
- 
-
-
-    z= -a2*np.sin(q2) - a3*np.sin(q2)*np.cos(q3) - a3*np.sin(q3)*np.cos(q2) + a4*(np.sin(q2)*np.sin(q3) - np.cos(q2)*np.cos(q3))*np.sin(q4) + a4*(-np.sin(q2)*np.cos(q3) - np.sin(q3)*np.cos(q2))*np.cos(q4) + d1
-
- 
-
-    return x,y,z
- * 
- */
-
-export type CinematicFunction = (coordinates: number[]) => { x: number, y: number, z: number }
+/* Our kinematic functions. 
+    Inputs:
+        Coordinates: Array of "NumJoints" positions. It contains the radians values of every joint.
+        Robotname: String containing the name of the robot.
+    Outputs:
+        x, y, z: The values of the end-effector's coordinates.
+*/
+export type CinematicFunction = (coordinates: number[], robotname:string) => { x: number, y: number, z: number }
 
 export const cinematicFunctions: Map<string, CinematicFunction> = new Map<string, CinematicFunction>(
     [
         ['EDScorbot', robotPointTo3D],
-        ['EDScorbotSim', robotPointTo3D]
+        ['EDScorbotSim', robotPointTo3D],
+        ['Rbtanno', robotPointTo3D]
+
     ]
 )
 
-function robotPointTo3D2(coordinates: number[]) {
-    return { x: 10, y: 10, z: 10 }
+
+//Auto-generates the transformation matrix with the DH parameters for a given joint index
+//Angles are in radians here
+function generateDHmatrix(index: number, angle: number, matrix: number[][])
+{
+    //DH matrix Edscorbot                       
+    const Alpha = matrix[index][0]; 
+    const Theta_Offset = matrix[index][1];  
+    const a = matrix[index][2]; 
+    const d = matrix[index][3]; 
+
+    var theta = angle + Theta_Offset;
+
+    var TF_matrix = 
+    [
+        [Math.cos(theta), -1 * Math.sin(theta) * Math.cos(Alpha), Math.sin(theta) * Math.sin(Alpha), a * Math.cos(theta)],
+        [Math.sin(theta), Math.cos(theta) * Math.cos(Alpha), -1 * Math.cos(theta) * Math.sin(Alpha), a * Math.sin(theta)],
+        [0, Math.sin(Alpha), Math.cos(Alpha), d],
+        [0, 0, 0, 1]
+    ]
+    
+    return TF_matrix;
 }
 
-function robotPointTo3D3(coordinates: number[]) {
-    return { x: 100, y: 50, z: 30 }
+//Simple matrix multiplication function
+function matrixMultiplication(matrixA: number[][], matrixB: number[][]) {
+
+    const rowsA = matrixA.length;
+    const colsA = matrixA[0].length;
+    const colsB = matrixB[0].length;
+
+    if (colsA !== matrixB.length) {
+        throw new Error("Matrix dimensions are not compatible for multiplication");
+    }
+
+    const result = new Array(rowsA);
+
+    for (let i = 0; i < rowsA; i++) {
+        result[i] = new Array(colsB);
+        for (let j = 0; j < colsB; j++) {
+            result[i][j] = 0;
+            for (let k = 0; k < colsA; k++) {
+                result[i][j] += matrixA[i][k] * matrixB[k][j];
+            }
+        }
+    }
+
+    return result;
 }
 
-//ANGLES ARE IN RADIANS ??? by the values it seems to be in degrees
-function robotPointTo3D(coordinates: number[]) {
-    const a1 = 5.0
-    const a2 = 30
-    const a3 = 35
-    const a4 = 22
-    const d1 = 35.85
-    const d2 = -9.8
-    const d3 = 6.5
+/*The actual kinematic function. It will evaluate the robot name before starting the calculations for xyz representation.
+In order for the function to work, both the robot name and the DH parameter matrix must be known and declared in this script of code
+Angles are given in degrees, and then transformed to radians
+*/
+function robotPointTo3D(coordinates: number[], robotname: string) {
 
-    var q1 = coordinates[0]
-    var q2 = coordinates[1]
-    var q3 = coordinates[2]
-    var q4 = coordinates[3]
+    var I_matrix = 
+    [
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ];
 
-    var x =
-        a1 * Math.cos(q1)
-        + a2 * Math.cos(q1) * Math.cos(q2)
-        - a3 * Math.sin(q2) * Math.sin(q3) * Math.cos(q1)
-        + a3 * Math.cos(q1) * Math.cos(q2) * Math.cos(q3)
-        + a4 * (- Math.sin(q2) * Math.sin(q3) * Math.cos(q1) + Math.cos(q1) * Math.cos(q2) * Math.cos(q3)) * Math.cos(q4)
-        + a4 * (- Math.sin(q2) * Math.cos(q1) * Math.cos(q3) - Math.sin(q3) * Math.cos(q1) * Math.cos(q2)) * Math.sin(q4)
-        - d2 * Math.sin(q1) - d3 * Math.sin(q1)
+    var TM = I_matrix;
 
-    var y =
-        a1 * Math.sin(q1)
-        + a2 * Math.sin(q1) * Math.cos(q2)
-        - a3 * Math.sin(q1) * Math.sin(q2) * Math.sin(q3)
-        + a3 * Math.sin(q1) * Math.cos(q2) * Math.cos(q3)
-        + a4 * (-Math.sin(q1) * Math.sin(q2) * Math.sin(q3) + Math.sin(q1) * Math.cos(q2) * Math.cos(q3)) * Math.cos(q4)
-        + a4 * (-Math.sin(q1) * Math.sin(q2) * Math.cos(q3) - Math.sin(q1) * Math.sin(q3) * Math.cos(q2)) * Math.sin(q4)
-        + d2 * Math.cos(q1)
-        + d3 * Math.cos(q1)
+    if (robotname == 'EDScorbot'){
+        for (let DOF = 0; DOF < DH_Edscorbot[0].length; DOF++) {
+            TM = generateDHmatrix(DOF, coordinates[DOF]*Math.PI/180, DH_Edscorbot);
+            I_matrix = matrixMultiplication(I_matrix,TM);
+        }
+    } 
+    else if (robotname == 'Rbtanno'){
+        for (let DOF = 0; DOF < DH_Robotanno[0].length; DOF++) {
+            TM = generateDHmatrix(DOF, coordinates[DOF]*Math.PI/180, DH_Robotanno);
+            I_matrix = matrixMultiplication(I_matrix,TM);
+        }
+    } 
 
-    var z =
-        - a2 * Math.sin(q2)
-        - a3 * Math.sin(q2) * Math.cos(q3)
-        - a3 * Math.sin(q3) * Math.cos(q2)
-        + a4 * (Math.sin(q2) * Math.sin(q3) - Math.cos(q2) * Math.cos(q3)) * Math.sin(q4)
-        + a4 * (-Math.sin(q2) * Math.cos(q3) - Math.sin(q3) * Math.cos(q2)) * Math.cos(q4)
-        + d1
-
-
-    return { x: x, y: y, z: z }
+    return { x: I_matrix[0][3], y: I_matrix[1][3], z: I_matrix[2][3] };
 }
+
+
+/*//Test the function with example coordinates
+//Test robotanno
+const coordinates_1 = [0, -90, 90, 0, 180, 0];
+const endEffectorPosition_1 = robotPointTo3D(coordinates_1,'Rbtanno');
+
+console.log("Coordinates (radians):", coordinates_1);
+console.log("End-Effector Position:");
+console.log("X:", endEffectorPosition_1.x);
+console.log("Y:", endEffectorPosition_1.y);
+console.log("Z:", endEffectorPosition_1.z);
+
+//Test edscorbot
+const coordinates_2 = [0, 0, 0, 0, 0, 0];
+const endEffectorPosition_2 = robotPointTo3D(coordinates_2,'EDScorbot');
+
+console.log("Coordinates (radians):", coordinates_2);
+console.log("End-Effector Position:");
+console.log("X:", endEffectorPosition_2.x);
+console.log("Y:", endEffectorPosition_2.y);
+console.log("Z:", endEffectorPosition_2.z);
+*/
+
+
+
